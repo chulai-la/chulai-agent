956e2b7bd591645fb319725f40f964c2dee65dec
SkyLothar <allothar@gmail.com>
2015-07-07 14:37:17 +0800

    add log config

--- deploy/gunicorn-server.py
+++ deploy/gunicorn-server.py
@@ -1,3 +1,7 @@
 import multiprocessing
+import os
+
+__curdir__ = os.path.dirname(os.path.realpath(__file__))
 
 workers = multiprocessing.cpu_count() * 2 + 1
+logconfig = os.path.join(__curdir__, "gunicorn-log.conf")
